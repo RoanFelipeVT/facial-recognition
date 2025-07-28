b2d631491b858e2dc9379a5809ee19ac874be477
RBergamaschi <rodrigotberga2121@hotmail.com>
2025-07-28 16:27:37 -0300

    deixando funcional com o front

--- src/infra/sqlalchemy/database.py
+++ src/infra/sqlalchemy/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Substitua pelas suas credenciais do MySQL
 # Ex: mysql+mysqlconnector://user:password@homst/database_name
-DATABASE_URL = "mysql+pymysql://root:Koraliny2406%40@localhost:3306/facialrecognition"
+DATABASE_URL = "mysql+pymysql://root:Koraliny2406%40@db:3306/facialrecognition"
 
 
 engine = create_engine(DATABASE_URL)
