9fb6ff9474790801c27f00b0cafdefb136656079
Nicolas Pinheiro <151929844+NickGomes1@users.noreply.github.com>
2025-07-29 21:16:27 -0300

    Update b14f80cfaed0_criar_tabelas_iniciais_a_partir_dos_.py

---
+++ alembic/versions/b14f80cfaed0_criar_tabelas_iniciais_a_partir_dos_.py
@@ -0,0 +1,16 @@
+"""Criar tabelas iniciais a partir dos modelos
+
+Revision ID: b14f80cfaed0
+Revises: 
+Create Date: 2025-07-29 20:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = 'b14f80cfaed0'
+down_revision = None
+branch_labels = None
+depends_on = None
