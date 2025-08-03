7eb097a2cfaa22634846d78ce754bebdf967efcd
Nicolas Pinheiro <151929844+NickGomes1@users.noreply.github.com>
2025-08-03 00:22:22 -0300

    Update main.py Origins modified

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
-
 from src.infra.sqlalchemy.database import Base, engine
 from src.infra.sqlalchemy.routes import admin, user, recognition, user_log
 
@@ -11,8 +10,8 @@ app = FastAPI(
     description="Backend para gerenciamento de usuários e reconhecimento facial com autenticação JWT para administradores."
 )
 
-# Permitir frontend
 origins = [
+    "https://nick-frontend-app.zxwnxt.easypanel.host",
     "http://localhost:3001",
 ]
 
@@ -24,21 +23,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Garantir que pasta de imagens exista
 if not os.path.exists("images"):
     os.makedirs("images")
 
-# Expor imagens publicamente
-app.mount("/images", StaticFiles(directory="images"), name="images")
-
-# Criar tabelas do banco
-Base.metadata.create_all(bind=engine)
-
-# Inclui as rotas
-app.include_router(admin.router)
-app.include_router(user.router)
-app.include_router(user_log.router)
-app.include_router(recognition.router) 
+app.include_router(admin.router, prefix="/api")
+app.include_router(user.router, prefix="/api")
+app.include_router(user_log.router, prefix="/api")
+app.include_router(recognition.router, prefix="/api")
 
 @app.get("/")
 def read_root():
