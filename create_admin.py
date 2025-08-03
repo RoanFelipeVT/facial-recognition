c9c2b240c92b810f16a9660ea755ef75fee5ea64
Nicolas Pinheiro <151929844+NickGomes1@users.noreply.github.com>
2025-08-03 03:30:45 -0300

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -4,7 +4,7 @@ import getpass
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from src.infra.sqlalchemy.repositories.admin_repository import AdminRepository
+from src.infra.sqlalchemy.repositories.admin import AdminRepository
 from src.schemas.admin_schema import AdminCreate
 from src.infra.sqlalchemy.database import SessionLocal
 
@@ -18,12 +18,9 @@ def create_admin_user():
     
     try:
         admin_data = AdminCreate(name=name, password=password)
-        
         repo = AdminRepository(db)
         repo.create_admin(admin_data)
-        
         print(f"\n[SUCESSO] Administrador '{name}' criado com sucesso!")
-        
     except Exception as e:
         print(f"\n[ERRO] Ocorreu um erro ao criar o administrador: {e}")
     finally:
